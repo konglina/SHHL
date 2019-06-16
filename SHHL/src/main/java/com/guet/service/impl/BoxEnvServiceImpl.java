4e31fd804e205118b7e60e25ba1a9882aa387f8d
liumanyi <2363330581@qq.com>
2019-06-16 10:05:41 +0800

    添加了BoxEnv的查询功能

---
+++ SHHL/src/main/java/com/guet/service/impl/BoxEnvServiceImpl.java
@@ -0,0 +1,22 @@
+package com.guet.service.impl;
+
+import java.util.List;
+import java.util.Map;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Service;
+
+import com.guet.dao.BoxEnvDao;
+import com.guet.entity.BoxEnv;
+import com.guet.service.BoxEnvService;
+@Service("boxEnvService")
+public class BoxEnvServiceImpl implements BoxEnvService{
+	@Resource
+	private BoxEnvDao boxEnvDao;
+	public List<BoxEnv> getList(Map<String, Object> map) {
+		// TODO Auto-generated method stub
+		return boxEnvDao.getList(map);
+	}
+
+}
