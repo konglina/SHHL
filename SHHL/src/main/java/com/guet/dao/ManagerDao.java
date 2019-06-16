28a350dca653b6fb31e88931d0eafcdd6afea955
MicahYin <yinwq887@163.com>
2019-06-16 10:45:02 +0800

    删除了自动生成的代码

--- SHHL/src/main/java/com/guet/dao/ManagerDao.java
+++
@@ -1,9 +0,0 @@
-package com.guet.dao;
-/**
- * 管理员Dao接口
- * @author guet
- *
- */
-public interface ManagerDao {
-	
-}
--- SHHL/src/main/java/com/guet/service/MangerService.java
+++
@@ -1,9 +0,0 @@
-package com.guet.service;
-/**
- * 管理员Service接口
- * @author guet
- *
- */
-public interface MangerService {
-	
-}
